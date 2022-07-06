ec2bb96e56b31e1102d47d5a0a6183584d430a4f
jonas <jonasriedel@protonmail.com>
2022-07-06 11:15:30 +0200

    Remove not working

--- arch/installYay.sh
+++ arch/installYay.sh
@@ -1,1 +1,1 @@
-sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && yes | makepkg -si && rm -rf .
+sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && yes | makepkg -si
